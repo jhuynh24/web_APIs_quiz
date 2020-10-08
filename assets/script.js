519ed7a9f3180392e4eae2876cece59359725a93
jhuynh24 <jhuynh1735@gmail.com>
2020-10-08 15:57:36 -0500

    adding endquiz function that creates a prompt to enter your initials once the quiz is finished. Also added code for accessing local storage in order to track high scores.

--- assets/script.js
+++ assets/script.js
@@ -12,55 +12,55 @@ var game = document.getElementById("game");
 var question = document.getElementById("question");
 var options = document.getElementById("options");
 var questionsArray = [{
-    question: "1. What is the main programming language used to make interactive web pages?",
-    choices: ["a. python", "b. java", "c. C++", "d. Javascript"],
-    answer: "d. Javascript"
-},
-{
-    question: "2. What syntax is used for single-line commenting in javascript?",
-    choices: ["a. ::", "b. //", "c. ||", "d. .."],
-    answer: "b. //"
-},
-{
-    question: "3. Which of the following languages is not one of the main three used for basic web development?",
-    choices: ["a. Javascript", "b. C#", "c. CSS", "d. HTML"],
-    answer: "b. C#"
-},
-{
-    question: "4. What is the correct way to externally link a .js file to an HTML file?",
-    choices: ["a. <script src='script.js'>", "b. <script href='script.js'>", "c. <link href='script.js'>", "d. <link src='script.js'"],
-    answer: "a. <script src='script.js'>"
-},
-{
-    question: "5. What syntax is used directly after a built in  Javascript method?",
-    choices: ["a. []", "b. {}", "c. **", "d. ()"],
-    answer: "d. ()"
-},
-{
-    question: "6. What does API stand for?",
-    choices: ["a. artificial programing intercom", "b. accessible programming interface", "c. application programming interface", "d. assigned programming interface"],
-    answer: "c. application programming interface"
-},
-{
-    question: "7. What type of equality operator is used for a strict equality (type and value)?",
-    choices: ["a. =", "b. ==", "c. ===", "d. +=="],
-    answer: "c. ==="
-},
-{
-    question: "8. What library is commonly used alongside CSS?",
-    choices: ["a. bootstrap", "b. jquery", "c. w3schools", "d. MDN"],
-    answer: "a. bootstrap"
-},
-{
-    question: "9. Boolean statements will return this type of value...",
-    choices: ["a. yes/no", "b. true/false", "c. num/var", "d. =/!="],
-    answer: "b. true/false"
-},
-{
-    question: "10. What type of loop gets executed as long as a condition is true?",
-    choices: ["a. for", "b. do", "c. while", "d. break"],
-    answer: "a. for"
-},
+        question: "1. What is the main programming language used to make interactive web pages?",
+        choices: ["a. python", "b. java", "c. C++", "d. Javascript"],
+        answer: "d. Javascript"
+    },
+    {
+        question: "2. What syntax is used for single-line commenting in javascript?",
+        choices: ["a. ::", "b. //", "c. ||", "d. .."],
+        answer: "b. //"
+    },
+    {
+        question: "3. Which of the following languages is not one of the main three used for basic web development?",
+        choices: ["a. Javascript", "b. C#", "c. CSS", "d. HTML"],
+        answer: "b. C#"
+    },
+    {
+        question: "4. What is the correct way to externally link a .js file to an HTML file?",
+        choices: ["a. <script src='script.js'>", "b. <script href='script.js'>", "c. <link href='script.js'>", "d. <link src='script.js'"],
+        answer: "a. <script src='script.js'>"
+    },
+    {
+        question: "5. What syntax is used directly after a built in  Javascript method?",
+        choices: ["a. []", "b. {}", "c. **", "d. ()"],
+        answer: "d. ()"
+    },
+    {
+        question: "6. What does API stand for?",
+        choices: ["a. artificial programing intercom", "b. accessible programming interface", "c. application programming interface", "d. assigned programming interface"],
+        answer: "c. application programming interface"
+    },
+    {
+        question: "7. What type of equality operator is used for a strict equality (type and value)?",
+        choices: ["a. =", "b. ==", "c. ===", "d. +=="],
+        answer: "c. ==="
+    },
+    {
+        question: "8. What library is commonly used alongside CSS?",
+        choices: ["a. bootstrap", "b. jquery", "c. w3schools", "d. MDN"],
+        answer: "a. bootstrap"
+    },
+    {
+        question: "9. Boolean statements will return this type of value...",
+        choices: ["a. yes/no", "b. true/false", "c. num/var", "d. =/!="],
+        answer: "b. true/false"
+    },
+    {
+        question: "10. What type of loop gets executed as long as a condition is true?",
+        choices: ["a. for", "b. do", "c. while", "d. break"],
+        answer: "a. for"
+    },
 ];
 
 
@@ -84,6 +84,33 @@ function countDown() {
         endQuiz();
     }
 }
+
+function endQuiz() {
+    console.log("End quiz function called.");
+    quizInstructions.remove();
+    game.classList.add("hide");
+    timeHolder.classList.add("hide");
+    quizHeader.textContent = "HIGH SCORES";
+    var initials = prompt("Please enter your initials to save your score.");
+    console.log(initials);
+    var currentScore = {
+        userInitials: initials,
+        userScore: numCorrect
+    };
+    var savedScores = localStorage.getItem('highscore') || '[]';
+    var listOfScores = [...JSON.parse(savedScores), currentScore];
+    listOfScores.sort((a, b) => b.userScore - a.userScore);
+    localStorage.setItem('highscore', JSON.stringify(listOfScores));
+    var scoreList = document.createElement("ol");
+    instructions.appendChild(scoreList);
+    for (var i = 0; i < listOfScores.length; i++) {
+        var listElement = document.createElement("li");
+        listElement.setAttribute("class", "listElement");
+        listElement.textContent = listOfScores[i].userScore + "-----" + listOfScores[i].userInitials;
+        scoreList.appendChild(listElement);
+    }
+}
+
 
 function newQuestion() {
     var currentQuestion = questionsArray[index];
