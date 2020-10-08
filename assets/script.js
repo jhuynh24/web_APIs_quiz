bf65755066b0440fef4c8908270d1243206ef94a
jhuynh24 <jhuynh1735@gmail.com>
2020-10-08 15:45:15 -0500

    Adding timer function to begin the countdown when the start button is pushed. Added more global variables.

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,32 @@ var timeHolder = document.getElementById("timeHolder");
 var instructions = document.getElementById("instructions");
 var quizHeader = document.getElementById("quizHeader");
 var quizInstructions = document.getElementById("quizInstructions");
-
+var timerDisplay = document.getElementById("timer");
+var timeLeft = 150;
+var downloadTimer;
+var index = 0;
+var numCorrect = 0;
+var game = document.getElementById("game");
+var question = document.getElementById("question");
+var options = document.getElementById("options");
 
 startButton.addEventListener("click", function () {
     startButton.classList.add("hide");
     timeHolder.classList.remove("hide");
     quizInstructions.classList.add("hide");
-
+    downloadTimer = setInterval(countDown, 1000);
+    newQuestion();
 });
+
+function countDown() {
+
+
+    timeLeft--;
+    timerDisplay.textContent = timeLeft;
+    if (timeLeft <= 0) {
+        timeLeft = 0;
+        timerDisplay.textContent = timeLeft;
+        clearInterval(downloadTimer);
+        endQuiz();
+    }
+}
